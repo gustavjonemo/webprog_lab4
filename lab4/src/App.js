1f55d98c91c253d7c270b833e94b14cfdb671d60
gustavjonemo <gustav-jonemo@hotmail.com>
2022-02-22 15:43:34 +0100

    Fetch data funkar

--- lab4/src/App.js
+++ lab4/src/App.js
@@ -63,14 +63,16 @@ export default class App extends Component {
     })
     console.log(orderData);
 
-    
+    let fetchData = {};
     fetch(API + 'orders/', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify(orderData)
-    });
+    }).then(response => response.json())
+    .then(data => fetchData = data)
+    .catch(error => console.log(error));
     
   }
 
